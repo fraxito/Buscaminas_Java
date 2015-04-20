7a0c0bb8065383758e263b0388f2e62e714f0a86
Jorge Cisneros <fraxito@gmail.com>
2015-04-20 11:02:31 +0200

    Buscaminas básico - sólo funciona la parte visual, no explotan las minas

---
+++ src/Boton.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JButton;
+
+
+
+
+public class Boton extends JButton{
+    
+    int bomba = 0;
+    int minasAlrededor = 0;
+    int x,y;
+    
+    public Boton (int _x, int _y){
+        x = _x;
+        y = _y;
+    }
+}
